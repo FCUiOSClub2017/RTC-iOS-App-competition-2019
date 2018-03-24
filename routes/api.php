530966682d04052c6af96aab13ad4c189269798a
KUI\ahkui <ahkui@outlook.com>
2018-03-24 22:46:15 +0800

    add deploy url

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('deploy', function () {
+    ini_set('max_execution_time', 0);
+    $cmd = 'cd /var/www;/usr/bin/git fetch origin 2>&1;/usr/bin/git reset --hard origin/master 2>&1;chmod -R 777 /var/www/storage;composer dump-autoload;php artisan clear-compiled;php artisan view:clear;php artisan config:clear;php artisan queue:restart;composer install';
+    exec($cmd, $output, $return);
+    if ($return !== 0) {
+        return response($output, 500);
+    }
+    $exitCode = Artisan::call('migrate');
+
+    return ['gitdeploy'=>$output, 'migrate'=>$exitCode];
 });
