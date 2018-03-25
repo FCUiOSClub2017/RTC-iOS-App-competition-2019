0bc1fe43c2e4f32f1a5ae121992ff7795930bf84
KUI\ahkui <ahkui@outlook.com>
2018-03-25 17:38:06 +0800

    update

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -12,13 +12,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ config('app.name', 'Coming Soon') }}</title>
-    @if (APP::envirament('server'))
-        <script src="{{$route_prefix}}/js/coming-soon.js" defer></script>
-        <link href="{{$route_prefix}}/css/coming-soon.css" rel="stylesheet"> 
-    @else
-        <script src="{{$route_prefix}}/js/coming-soon.js" defer></script>
-        <link href="{{$route_prefix}}/css/coming-soon.css" rel="stylesheet">
-    @endif
+    <script src="{{$route_prefix}}/js/coming-soon.js" defer></script>
+    <link href="{{$route_prefix}}/css/coming-soon.css" rel="stylesheet">
 </head>
 
 <body>
