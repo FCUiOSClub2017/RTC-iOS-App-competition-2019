572a6c2d0a89819badc051b295a9b3542e31f3ff
KUI\ahkui <ahkui@outlook.com>
2018-03-25 18:12:34 +0800

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -46,7 +47,7 @@ class LoginController extends Controller
      */
     public function logout(Request $request)
     {
-        parent::logout();
+        parent::logout($request);
         return redirect('/2018');
     }
 
