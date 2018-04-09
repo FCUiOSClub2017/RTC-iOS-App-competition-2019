7926538c31aae21b7c90d44a44ab0b6577220660
KUI\ahkui <ahkui@outlook.com>
2018-04-09 21:34:40 +0800

    update timeline

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -5,7 +5,7 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>4/30</a></span>
+            <span class="stats-no"><a>5/08</a></span>
             <div class="circle-overflow">
               <div class="circle-content">
                 <a>報名截止</a>
@@ -17,12 +17,11 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>5/15</a></span>
+            <span class="stats-no"><a>5/23</a></span>
             <div class="circle-overflow">
               <div class="circle-content">
-                <a>書面
-                  <br/>企劃案
-                  <br/>提交截止</a>
+                <a>作品
+                  <br/>繳交截止</a>
               </div>
             </div>
           </div>
@@ -32,10 +31,10 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>6/8</a></span>
+            <span class="stats-no"><a>6/15</a></span>
             <div class="circle-overflow">
               <div class="circle-content">
-                <a>初選結果公布</a>
+                <a>初賽結果公布</a>
               </div>
             </div>
           </div>
@@ -46,13 +45,13 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>6/24</a></span>
+            <span class="stats-no"><a>7/04</a></span>
             <div class="circle-overflow">
               <div class="circle-content">
-                <a>交流討論會</a>
+                <a>工作坊</a>
               </div>
               <div class="circle-overlay">
-                <a>於逢甲大學進行Workshop交流討論會,凡進入決賽的參賽隊伍皆可自由參加。</a>
+                <a>於逢甲大學進行 Workshop 交流討論會,凡進入決賽的參賽隊伍皆可自由參加。</a>
               </div>
             </div>
           </div>
@@ -61,7 +60,7 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>7/18</a></span>
+            <span class="stats-no"><a>8/02</a></span>
             <div class="circle-overflow">
               <div class="circle-content">
                 <a>決賽</a>
