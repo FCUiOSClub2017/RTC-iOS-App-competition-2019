293a124072c9f6f92f1309c6d04ff1dc0e310b98
KUI\ahkui <ahkui@outlook.com>
2018-04-16 16:55:46 +0800

    update sponsors

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -6,44 +6,48 @@
   </div>
   <div class="portfolio-grid">
     <div class="container">
-      <div class="row justify-content-around">{{--
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3">
+      <div class="row justify-content-around">
+        <div class="col-12 col-lg-4 col-sm-6 col-xl-4">
           <div class="card card-block">
-            <a href="#" class="sponsors sigmu"></a>
+            <a href="https://www.studioa.com.tw/" class="sponsors studioa"></a>
           </div>
         </div>
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3">
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="#" class="sponsors litenet"></a>
-          </div>
-        </div> {{-- --}}
-        <div class="col-12 col-lg-4 col-sm-6 col-xl-3">
-          <div class="card card-block">
-            <a href="#" class="sponsors studioa"></a>
-          </div>
-        </div>{{--
-        <div class="w-100 d-none d-xl-block"></div>
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3 push-3">
-          <div class="card card-block">
-            <a href="#" class="sponsors atomax"></a>
+            <a href="http://www.myvita.com.tw/" class="sponsors sigmu"></a>
           </div>
         </div>
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3">
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="#" class="sponsors fora"></a>
+            <a href="http://litenet.com.tw/" class="sponsors litenet"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
+          <div class="card card-block">
+            <a href="http://www.foracare.com.tw/" class="sponsors fora"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
+          <div class="card card-block">
+            <a href="http://www.eql.com.tw/" class="sponsors eql"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
+          <div class="card card-block">
+            <a href="http://shop.opro9.com/v2/official" class="sponsors opro9"></a>
           </div>
         </div>
         <div class="w-100 d-none d-lg-block d-xl-none"></div>
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3">
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="#" class="sponsors opro9"></a>
+            <a href="https://www.pawbo.com/tw/" class="sponsors pawbo"></a>
           </div>
         </div>
-        <div class="col-12 col-lg-4 col-md-6 col-xl-3">
+        <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="#" class="sponsors qblinks"></a>
+            <a href="https://mytracmo.com/pages/tracmo-taiwan" class="sponsors tracmo"></a>
           </div>
-        </div> {{-- --}}
+        </div>
       </div>
     </div>
   </div>
