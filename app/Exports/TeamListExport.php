a0ae402c27823f25eb649217370888972d79806d
ahkui <ahkui@outlook.com>
2018-04-23 06:12:10 +0000

    Apply fixes from StyleCI

--- app/Exports/TeamListExport.php
+++ app/Exports/TeamListExport.php
@@ -2,12 +2,10 @@
 
 namespace App\Exports;
 
+use App\TeamMember;
+use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
-use App\User;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
-use App\TeamMember;
 
 class TeamListExport implements FromCollection, WithHeadings
 {
@@ -16,27 +14,27 @@ class TeamListExport implements FromCollection, WithHeadings
         $data = User::role('participant')->with('team_member')->get();
         $alldata = collect();
         // dd($data->all());
-        
+
         foreach ($data as $teamkey => $teamvalue) {
             $teamdata = collect([
-                'created_at'=>$teamvalue->created_at->toDateTimeString(),
-                'team_name'=>$teamvalue->name,
-                'team_email'=>$teamvalue->email,
-                'split'=>'',
+                'created_at'=> $teamvalue->created_at->toDateTimeString(),
+                'team_name' => $teamvalue->name,
+                'team_email'=> $teamvalue->email,
+                'split'     => '',
             ]);
-            for ($i=1;$i<6;$i++) {
+            for ($i = 1; $i < 6; $i++) {
                 $value = $teamvalue->team_member()->whereLevel($i)->first();
-                if ($value)
+                if ($value) {
                     $teamdata->push([
-                        'level'=>TeamMember::levelText($value->level),
-                        'school'=>$value->univercity->name,
-                        'course'=>$value->univercity->course,
-                        'name'=>$value->name,
-                        'email'=>$value->email,
-                        'phone'=>$value->phone,
-                        'split'=>'',
+                        'level' => TeamMember::levelText($value->level),
+                        'school'=> $value->univercity->name,
+                        'course'=> $value->univercity->course,
+                        'name'  => $value->name,
+                        'email' => $value->email,
+                        'phone' => $value->phone,
+                        'split' => '',
                     ]);
-                else
+                } else {
                     $teamdata->push([
                         '',
                         '',
@@ -46,6 +44,7 @@ class TeamListExport implements FromCollection, WithHeadings
                         '',
                         '',
                     ]);
+                }
             }
             $alldata->push($teamdata->flatten());
         }
