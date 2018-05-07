6c14836027da2f3d366388e28d53849111e599c6
ahkui <ahkui@outlook.com>
2018-05-07 07:56:10 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/DeadlineOfRegister.php
+++ app/Http/Middleware/DeadlineOfRegister.php
@@ -2,22 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Carbon\Carbon;
+use Closure;
 
 class DeadlineOfRegister
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(Carbon::create(2018, 5, 16, 0, 0, 0)->lt(Carbon::now()))
+        if (Carbon::create(2018, 5, 16, 0, 0, 0)->lt(Carbon::now())) {
             return redirect()->route('login');
+        }
+
         return $next($request);
     }
 }
