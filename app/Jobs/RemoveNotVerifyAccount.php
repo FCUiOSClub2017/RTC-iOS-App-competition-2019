7e541b35c239c582d9b7e4c7e786b1c674daff02
KUI\ahkui <ahkui@outlook.com>
2018-05-11 23:46:09 +0800

    update carbon fail

--- app/Jobs/RemoveNotVerifyAccount.php
+++ app/Jobs/RemoveNotVerifyAccount.php
@@ -3,7 +3,7 @@
 namespace App\Jobs;
 
 use App\User;
-use Carbon;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
