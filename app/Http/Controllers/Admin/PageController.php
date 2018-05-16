9c4cf35b6a236c43c98fbbc773983556f052b310
ahkui <ahkui@outlook.com>
2018-05-16 15:54:46 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
-class EditPageController extends Controller
+class PageController extends Controller
 {
     /**
      * Create a new controller instance and set middleware.
