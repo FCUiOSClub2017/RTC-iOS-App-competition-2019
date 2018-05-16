3c169e964f8c250ddb07671df0d2914f665d944c
KUI\ahkui <ahkui@outlook.com>
2018-05-17 00:20:09 +0800

    change blade content date to dynamic

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -48,7 +48,7 @@
                 <a>初賽結果公布</a>
               </div>
               <div class="circle-overlay">
-                <a>獲選進入決賽之隊伍，需於6/30前繳交，所在學校系所用印後的報名表如附件二，文件需整併成一個PDF檔,檔案名稱須同【聯絡人學校_聯絡 人系級_聯絡人姓名_參賽APP名稱】。</a>
+                <a>獲選進入決賽之隊伍，需於 {{Carbon::parse(Setting::get('register_form_deadline', '2018-06-30'), 'Asia/Taipei')->month}}/{{Carbon::parse(Setting::get('register_form_deadline', '2018-06-30'), 'Asia/Taipei')->day}} 前繳交，所在學校系所用印後的報名表如附件二，文件需整併成一個PDF檔,檔案名稱須同【聯絡人學校_聯絡 人系級_聯絡人姓名_參賽APP名稱】。</a>
                 <a href="/doc/2018年APP移動應用創新賽附件1~3.pdf" style=" top:90%;color:aqua;" onclick="event.preventDefault();window.open('/doc/2018年APP移動應用創新賽附件1~3.pdf', '_blank');">附件</a>
                 {{--
                 <a>獲選進入決賽之隊伍，必須在一周內提供每位成員的在學證明以及指導老師的教師資格證明</a> --}}
@@ -62,7 +62,7 @@
       <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
         <div class="ratio-1-1">
           <div class="circle">
-            <span class="stats-no"><a>7/20</a></span>
+            <span class="stats-no"><a>{{Carbon::parse(Setting::get('app_upload_deadline', '2018-07-20'), 'Asia/Taipei')->month}}/{{Carbon::parse(Setting::get('app_upload_deadline', '2018-07-20'), 'Asia/Taipei')->day}}</a></span>
             <div class="circle-overflow" data-toggle="modal-hide" data-target="#date7-04">
               <div class="circle-content">
                 <a>APP 作品
