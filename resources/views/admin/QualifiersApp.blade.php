8b266d2cfdd860fa3416a5a741f3dfec5d0b3c91
ahkui <14049597+ahkui@users.noreply.github.com>
2018-07-20 19:20:59 +0800

    Update QualifiersApp.blade.php

--- resources/views/admin/QualifiersApp.blade.php
+++ resources/views/admin/QualifiersApp.blade.php
@@ -3,6 +3,7 @@
     <div class="row">
         <div class="col">
             <div class="list-group">
+            <a class="text-white list-group-item list-group-item-action active">已繳交檔案的組別</a>
                 @foreach($users as $user)
                 <a href="{{route('admin.team.qualifiers.app.download',$user)}}" class="list-group-item list-group-item-action">第{{$user}}組</a>
                 @endforeach
