bf960cb47499e90d97358fac5ea39a538aedec92
ahkui <14049597+ahkui@users.noreply.github.com>
2018-07-20 19:19:35 +0800

    Update QualifiersApp.blade.php

--- resources/views/admin/QualifiersApp.blade.php
+++ resources/views/admin/QualifiersApp.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.app') @section('content') 
-{{dump($users)}}
+<div class="container py-3">
+    <div class="row">
+        <div class="col">
+            <div class="list-group">
+                @foreach($users as $user)
+                <a href="{{route('admin.team.qualifiers.app.list',$user)}}" class="list-group-item list-group-item-action">第{{$user}}組</a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
