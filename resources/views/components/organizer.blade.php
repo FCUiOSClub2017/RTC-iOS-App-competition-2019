21eacd98d2923c286a624dadd7de3ee4179b45a3
xXHerngXx <sbherng1997@live.com>
2019-05-04 12:13:18 +0800

    renabled and added sponsor

--- resources/views/components/organizer.blade.php
+++ resources/views/components/organizer.blade.php
@@ -27,21 +27,21 @@
           </div>
           <div class="container">
             <div class="row justify-content-around">
-              <div class="col-12 col-md-6">
+              <div class="col-12 col-md-4">
                 <div class="card card-block">
                   <a href="http://www.apple.com/" class="sponsors apple"></a>
                 </div>
               </div>
-              <div class="col-12 col-md-6">
+              <div class="col-12 col-md-4">
                 <div class="card card-block">
                   <a href="http://www.fcu.edu.tw/" class="sponsors fcu"></a>
                 </div>
               </div>
-              <!-- <div class="col-12 col-md-4">
+              <div class="col-12 col-md-4">
                 <div class="card card-block">
                   <a href="https://www.tku.edu.tw/" class="sponsors tku"></a>
                 </div>
-              </div> -->
+              </div>
             </div>
           </div>
         </div>
--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -9,33 +9,86 @@
       <div class="row justify-content-around">
         <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="http://www.myvita.com.tw/" class="sponsors sigmu"></a>
+            <a href="http://smarthome.myvita.com.tw" class="sponsors infinite"></a>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-sm-6 col-xl-4">
           <div class="card card-block">
-            <a href="https://www.studioa.com.tw/" class="sponsors studioa"></a>
+            <a href="http://shop.opro9.com/v2/official" class="sponsors oprog"></a>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="http://www.taidoc.com.tw/" class="sponsors taidoc"></a>
+            <a href="https://www.studioa.com.tw/categories/uag" class="sponsors uag"></a>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="http://www.eql.com.tw/" class="sponsors eql"></a>
+            <a href="https://rhinoshield.tw/pages/solidsuit" class="sponsors rhino"></a>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="http://shop.opro9.com/v2/official" class="sponsors opro9"></a>
+            <a href="http://smarthome.myvita.com.tw" class="sponsors litenet"></a>
           </div>
         </div>
-        <div class="w-100 d-none d-lg-block d-xl-none"></div>
         <div class="col-12 col-lg-4 col-md-6 col-xl-4">
           <div class="card card-block">
-            <a href="https://www.pawbo.com/tw/" class="sponsors pawbo"></a>
+            <a href="https://www.studioa.com.tw" class="sponsors studioa"></a>
+          </div>
+        </div>
+
+
+
+
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="https://www.audio-technica.com.tw" class="sponsors audiotech"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://www.glowmax.com.tw/product.php?act=list&cid=160" class="sponsors speck"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://tw.pqigroup.com" class="sponsors pqi"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://www.elecom.com.tw" class="sponsors elecom"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="www.spigen.com.tw" class="sponsors spigen"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="www.ivokamo.com" class="sponsors vokamo"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://www.simplewear.com.tw" class="sponsors nomad"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://maxmikeco.com/" class="sponsors laut"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="http://www.yingdda.com/" class="sponsors jbl"></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-3 col-md-6 col-xl-3">
+          <div class="card card-block">
+            <a href="https://mytracmo.com/pages/tracmo-taiwan" class="sponsors tracmo"></a>
           </div>
         </div>
        <!--  <div class="col-12 col-lg-4 col-md-6 col-xl-3">
